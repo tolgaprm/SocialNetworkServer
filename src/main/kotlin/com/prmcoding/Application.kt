ed35517f80febdd5c79c921e02869abfc7c77956
Tolga Pirim <tolgapirim25@gmail.com>
2022-10-21 10:40:36 +0300

    Initial commit

---
+++ src/main/kotlin/com/prmcoding/Application.kt
@@ -0,0 +1,15 @@
+package com.prmcoding
+
+import io.ktor.server.application.*
+import com.prmcoding.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
